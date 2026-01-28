fada659eafc8dd8fbbaf0fbb3064f5163cea284f
Hunter Clezie <clezhunt@gmail.com>
2026-01-27 20:48:26 -0700

    Add code to differentiate names

--- SayMyNameAgain/Program.cs
+++ SayMyNameAgain/Program.cs
@@ -11,7 +11,30 @@ namespace SayMyNameAgain
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hi there, what is your name?");
+            string userinput = "Insert name";
+            userinput = Console.ReadLine();
+            switch (userinput)
+            {
+            case "Emily":
+                Console.WriteLine($"Hey {userinput}! How's your day been?");
+                break;
+            case "Joe":
+                Console.WriteLine($"Hehehehe wassup {userinput}");
+                break;
+            case "Hunter":
+                Console.WriteLine($"Yooooo *Robotically daps you up* my main man {userinput}");
+                break;
+            case "Tim":
+                Console.WriteLine($"TIMMMMM!!! Omg it's the Tim himself! I could literally scream right now! *Screams in beeps*");
+                Console.Beep(550,3000);
+                break;
+            default:
+                Console.WriteLine($"It's nice to meet you!");
+                break;
+            }
+                //pause
+                Console.Read();
         }
     }
 }
